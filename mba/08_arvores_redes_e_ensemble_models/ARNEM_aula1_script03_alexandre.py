8dd12e72f92a9e18e4d10f81fc6aec53367d2817
Alexandre Castelli <alexandrecastelli@hotmail.com>
2025-01-29 20:07:04 +0000

    Refactor decision tree implementation for clarity and consistency

--- mba/08_arvores_redes_e_ensemble_models/ARNEM_aula1_script03_alexandre.py
+++ mba/08_arvores_redes_e_ensemble_models/ARNEM_aula1_script03_alexandre.py
@@ -1,49 +1,52 @@
-
 # carrega as bibliotecas necessárias
-import seaborn as sns
-
+import pandas as pd
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score
+from funcoes_ajuda import descritiva, avalia_clf
 
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+# carrega os dados
+df = pd.read_parquet('exercicio.parquet')
 
-from sklearn.metrics import roc_auc_score, accuracy_score, classification_report, confusion_matrix, balanced_accuracy_score
+# verifica parte dos dados
+print(df.info())
+print(df.head())
+
+# verifica as estatísticas descritivas básicas
+for var in df.columns:
+    descritiva(df, var=var, vresp = 'inadimplencia')
 
 # seleciona as features e a target
-X = dados.drop(columns = ['inadimplencia'])
-y = dados['inadimplencia']
+X = df.drop(columns = ['inadimplencia'])
+y = df['inadimplencia']
 
 # define as bases de treino e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # confere o número de linhas e colunas
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
-
-# cria o modelo de árvore de decisão
-arvore = DecisionTreeClassifier(criterion='gini', 
-                                max_depth=30, 
-                                random_state=42,
-                                ccp_alpha=0)
+print("X_train shape:", X_train.shape)
+print("y_train shape:", y_train.shape)
+print("X_test shape:", X_test.shape)
+print("y_test shape:", y_test.shape)
 
 # treina o modelo
-arvore.fit(X_train, y_train)
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
 
 # cost-complexity pruning path
-ccp_path = pd.DataFrame(arvore.cost_complexity_pruning_path(X_train, y_train))
+ccp_path = pd.DataFrame(clf.cost_complexity_pruning_path(X_train, y_train))
 
 # tunando a árvore
 GINIs = []
 
 for ccp in ccp_path['ccp_alphas']:
-    arvore = DecisionTreeClassifier(criterion='gini', max_depth=30, random_state=42, ccp_alpha=ccp)
+    clf = DecisionTreeClassifier(criterion='gini', max_depth=30, random_state=42, ccp_alpha=ccp)
 
     # treina o modelo
-    arvore.fit(X_train, y_train)
-    AUC = roc_auc_score(y_test, arvore.predict_proba(X_test)[:, -1])
+    clf.fit(X_train, y_train)
+    AUC = roc_auc_score(y_test, clf.predict_proba(X_test)[:, -1])
     GINI = (AUC-0.5)*2
     GINIs.append(GINI)
 
@@ -60,25 +63,25 @@ plt.title('Avaliação da árvore por valor de CCP-Alpha')
 
 print(f'O GINI máximo é de: {GINI_max:.2%}\nObtido com um ccp de: {ccp_max}')
 
-# arvore ótima
-arvore = DecisionTreeClassifier(criterion='gini',
+# árvore ótima
+clf = DecisionTreeClassifier(criterion='gini',
                                 max_depth = 30, 
                                 random_state=42,
                                 ccp_alpha=ccp_max).fit(X_train, y_train)
 
 # plota a árvore
 plt.figure(figsize=(20, 10))
-plot_tree(arvore, feature_names=X.columns.tolist(), class_names=['Adimplente', 'Inadimplente'], filled=True)
+plot_tree(clf, feature_names=X.columns.tolist(), class_names=['Adimplente', 'Inadimplente'], filled=True)
 plt.show()
 
 # confusion_matrix faz a comparação por uma tabela cruzada
-cm = confusion_matrix(y, arvore.predict(X))
+cm = confusion_matrix(y, clf.predict(X))
 
 # accuracy_score calcula o percentual de acertos
-ac = accuracy_score(y, arvore.predict(X))
+ac = accuracy_score(y, clf.predict(X))
 
 # pondera para forçar a distribuição da target como uniforme
-bac = balanced_accuracy_score(y, arvore.predict(X))
+bac = balanced_accuracy_score(y, clf.predict(X))
 
 # mostra os resultados
 print(f'\nA acurácia da árvore é: {ac:.1%}')
@@ -92,4 +95,4 @@ sns.heatmap(cm,
 plt.show()
 
 # relatório de classificação do scikit
-print('\n', classification_report(y, arvore.predict(X)))
+print('\n', classification_report(y, clf.predict(X)))
